953e53c80e112743347936cafeae5e6e2f49990e
lore-lml <lorenzo.limoli@studenti.polito.it>
2018-03-26 16:02:56 +0200

    Lab_2 da rifinire 2 e 3 e fare il 4

---
+++ Lab_2.3.py
@@ -0,0 +1,47 @@
+from sys import argv
+filename = argv[1]
+txt = open(filename)
+tasks=txt.read().split("\n")
+txt.close()
+exit=0
+ordered=0
+while exit==0:
+    print("Scegli una delle seguenti opzioni:")
+    print("1) Inserisci nuovo task")
+    print("2) Rimuovi task")
+    print("3) Mostra task esistenti")
+    print("4) Elimina i task con una specifica sottostringa")
+    print("5) Esci")
+
+    cmd=int(input())
+    if cmd == 1:
+        print("Inserisci nome")
+        new_task=input()
+        tasks.append(new_task)
+        ordered=0
+    elif cmd == 2:
+        print("Quale task vuoi rimuovere?")
+        rm_task=input()
+        tasks.remove(rm_task)
+    elif cmd == 3:
+        if ordered==0:
+            tasks.sort()
+            ordered=1
+        print("")
+        for element in tasks:
+            print (element)
+        print("")
+    elif cmd == 4:
+        print("Che sottostringa devo cercare?")
+        substring=input()
+        for element in tasks:
+            if element.find(substring) != -1:
+                tasks.remove(element)
+    elif cmd == 5:
+        exit=1
+        out=open(filename,"w")
+        for element in tasks:
+            out.write(element + "\n")
+        out.close()
+    else:
+        print("Comando non riconosciuto")
